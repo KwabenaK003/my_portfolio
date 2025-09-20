babbf78c0e313871da19d6004014b896c9770e22
KwabenaK003 <kkboahene003@gmail.com>
2025-09-20 12:41:51 +0100

    more updates

---
+++ ddd.py
@@ -0,0 +1,14 @@
+
+        # .skill-category-card {
+        #     background-color: #2a2a2a;
+        #     padding: 30px;
+        #     border-radius: 10px;
+        #     box-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
+        #     text-align: center;
+        #     transition: transform 0.3s ease, box-shadow 0.3s ease;
+        # }
+
+        # .skill-category-card:hover {
+        #     transform: translateY(-5px);
+        #     box-shadow: 0 8px 20px rgba(0, 0, 0, 0.6);
+        # }
